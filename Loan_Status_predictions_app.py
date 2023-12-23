cf066b817641910379dc319efe0e967a0b960121
Muhammad Sheraz <sheraz601050@gmail.com>
2023-12-23 07:40:04 +0500

    code updated

--- Loan_Status_predictions_app.py
+++ Loan_Status_predictions_app.py
@@ -1,16 +1,7 @@
-# Import necessary libraries
 import streamlit as st
 import pandas as pd
 import pickle
 
-# Function to load the pre-trained model
-def load_model():
-    # Load the pre-trained model from the pickle file
-    with open('Loan Status predictions.pkl', 'rb') as model_file:
-        model = pickle.load(model_file)
-    return model
-
-# Streamlit app
 def main():
     with open('Loan Status predictions.pkl', 'rb') as model_file:
         model = pickle.load(model_file)
@@ -18,16 +9,14 @@ def main():
 
     st.sidebar.header('User Input Features')
 
-    # Collect user input
     married = st.sidebar.selectbox('Married', ['Yes', 'No'])
     education = st.sidebar.selectbox('Education', ['Graduate', 'Not Graduate'])
     applicant_income = st.sidebar.number_input('Applicant Income')
     coapplicant_income = st.sidebar.number_input('Coapplicant Income')
     loan_amount = st.sidebar.number_input('Loan Amount')
-    credit_history = st.sidebar.selectbox('Credit History', [0, 1])
+    credit_history = st.sidebar.selectbox('Credit History', [0.0, 1.0])
     property_area = st.sidebar.selectbox('Property Area', ['Urban', 'Semiurban', 'Rural'])
 
-    # Make prediction
     input_data = {
         'Married': [married],
         'Education': [education],
@@ -40,13 +29,9 @@ def main():
 
     input_df = pd.DataFrame(input_data)
 
-    # Preprocess user input
-    input_df = pd.get_dummies(input_df, columns=['Married', 'Education', 'Property_Area'], drop_first=True)
 
-    # Make prediction
     prediction = model.predict(input_df)
 
-    # Display prediction
     st.subheader('Prediction')
     st.write(f'The loan status is predicted to be: {prediction[0]}')
 
